9d9d7d09caf071c1e227df34f68ab38dfd8a4b6a
slarribeau <slarribeau@gmail.com>
2018-11-17 16:51:33 -0800

    SongList is limping

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import actions from '../actions';
-
+import SongList from './SongList'; 
 const App = () => {
-    return <div>App</div>;
-}
+    return(
+        <div>
+            <SongList />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/SongList.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'; //Use curly braces for named export (not default)
+
+
+class SongList extends Component {
+   render() {
+       return <div>SongList</div>;
+   }
+};
+
+export default SongList;
