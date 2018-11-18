8e6d4ea52ffcbe2981a7718cd6a41ba5d2344633
slarribeau <slarribeau@gmail.com>
2018-11-17 21:19:57 -0800

    make it purty

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,13 +2,28 @@ import React, {Component} from 'react'; //Use curly braces for named export (not
 import {connect} from 'react-redux'
 
 class SongList extends Component {
-   render() {
-       // this.props === {songs: state.songs}
-       console.log("2")
-       console.log(this.props);
-       console.log(this.state);
-       return <div>SongList</div>;
-   }
+
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+    render() {
+        // this.props === {songs: state.songs}
+        console.log("2")
+        console.log(this.props);
+        console.log(this.state);
+        return <div className="ui divided list">{this.renderList()}</div>;
+    }
 };
 
 const mapStateToProps = (state) => {
