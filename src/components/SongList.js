a82fbda3cbfcd94a0201b49a7b6caa37b8e70dc6
slarribeau <slarribeau@gmail.com>
2018-11-17 21:05:24 -0800

    bind redux to react

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'; //Use curly braces for named export (not default)
-
+import {connect} from 'react-redux'
 
 class SongList extends Component {
    render() {
+       // this.props === {songs: state.songs}
+       console.log("2")
+       console.log(this.props);
+       console.log(this.state);
        return <div>SongList</div>;
    }
 };
 
-export default SongList;
+const mapStateToProps = (state) => {
+   console.log("1")
+    console.log(state);
+   return {songs: state.songs};
+}
+
+export default connect(mapStateToProps)(SongList);
